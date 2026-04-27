d535aab1bb08aba22e3ed67c2b42cbbc17f966a9
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2026-04-27 19:49:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,14 +56,20 @@ function filterFiles(type) {
 
 const toggleBtn = document.getElementById("themeToggle");
 
-toggleBtn.addEventListener("click", () => {
-  console.log("Button clicked"); // 👈 ADD THIS
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggleBtn.textContent = "☀️ Light Mode";
+}
 
+toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
   if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
     toggleBtn.textContent = "☀️ Light Mode";
   } else {
+    localStorage.setItem("theme", "light");
     toggleBtn.textContent = "🌙 Dark Mode";
   }
 });
